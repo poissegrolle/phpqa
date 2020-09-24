effea00c2a2ac08818122814953e65c6ac67bc3a
Jean-Marie Le Bonniec <jmlebonniec@gmail.com>
2020-09-24 16:47:47 +0200

    twig with the right namespace

--- src/report.php
+++ src/report.php
@@ -5,13 +5,13 @@ namespace Edge\QA;
 use DOMDocument;
 use XSLTProcessor;
 use Exception;
-use Twig_Loader_Filesystem;
-use Twig_Environment;
+use Twig\Loader\Filesystem;
+use Twig\Environment;
 
 function twigToHtml($template, array $params, $outputFile)
 {
-    $loader = new Twig_Loader_Filesystem(__DIR__ . '/../app/report');
-    $twig = new Twig_Environment($loader);
+    $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../app/report');
+    $twig = new \Twig\Environment($loader);
     $html = $twig->render($template, $params);
     file_put_contents($outputFile, $html);
 }
